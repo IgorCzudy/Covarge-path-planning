8ac1cad2a7ac8cf543ce207c1056c8a12eca067d
IgorCzudy <iczudy@gmail.com>
2024-11-14 11:36:24 +0100

    zig_zag refactor for OOP

--- zig_zag.py
+++ zig_zag.py
@@ -1,105 +1,157 @@
-import numpy as np 
-from typing import List
+import numpy as np
+from typing import List, Tuple
 import networkx as nx
 
 
+class Zig_zag:
+    def __init__(self):
+        self.grid = None
+        self.x_shape = None
+        self.y_shape = None
+        self.complete_graph = None
+        self.nodes_without_obst = None
 
-def going_down(x, y, grid, x_shape, y_shape, path):
-    
-    indx = (y+1)*x_shape + x
-    path.append(indx) # add down 
-    y+= 1
-    return x, y, path
+        self.path = None
+        self.x = None
+        self.y = None
 
-    
-def can_i_go_down(x, y, y_shape, x_shape, grid, path):
-    if y+1 < y_shape and grid[y+1, x] == 0 and \
-        (y+1)*x_shape + x not in path:    #chck if down is ok 
-        return True
-    
-    return False
+    def _can_i_go_down(self):
+        return (
+            self.y + 1 < self.y_shape
+            and self.grid[self.y + 1, self.x] == 0
+            and (self.y + 1) * self.x_shape + self.x not in self.path
+        )
 
-def can_i_go_up(x, y, x_shape, grid, path):
-    if y-1 >=0 and grid[y-1, x] == 0 and (y-1)*x_shape + x not in path:
-        return True
-    return False
+    def _going_down(self):
+        indx = (self.y + 1) * self.x_shape + self.x
+        self.y += 1
+        self.path.append(indx)
 
+    def _can_i_go_up(self):
+        return (
+            self.y - 1 >= 0
+            and self.grid[self.y - 1, self.x] == 0
+            and (self.y - 1) * self.x_shape + self.x not in self.path
+        )
 
-def going_up(x, y, grid, x_shape, y_shape, path):
-    indx = (y-1)*x_shape + x
-    path.append(indx) # add down 
-    y-=1
-    return x, y, path
+    def _going_up(self):
+        indx = (self.y - 1) * self.x_shape + self.x
+        self.path.append(indx)
+        self.y -= 1
 
-def find_the_closest_one(x, y, x_shape, complete_graph, path):
-    indx = y*x_shape + x
-    filtered_connected_nodes_with_weights = [(neighbor, complete_graph[indx][neighbor].get('weight', 1)) for neighbor in complete_graph.neighbors(indx) if neighbor not in path]
-    next_indx, _ = min(filtered_connected_nodes_with_weights, key=lambda x: x[1])
-    
-    y = next_indx // x_shape
-    x = next_indx % x_shape
-    return x, y, next_indx, indx
+    def _can_i__go_right(self):
+        return (
+            self.x + 1 < self.x_shape
+            and self.grid[self.y, self.x + 1] == 0
+            and self.y * self.x_shape + (self.x + 1) not in self.path
+        )
 
+    def _go_right(self):
+        indx = self.y * self.x_shape + (self.x + 1)
+        self.path.append(indx)
+        self.x += 1
 
-def zig_zag_path(grid: np.ndarray, complete_graph: nx.Graph, nodes_without_obst: List[int]) -> List[int]:
-    
-    x_shape, y_shape = grid.shape
-    path = [0] #starting from node 0 - (0,0)
+    def _can_i__go_left(self):
+        return (
+            self.x > 0
+            and self.grid[self.y, self.x - 1] == 0
+            and self.y * self.x_shape + (self.x - 1) not in self.path
+        )
 
-    x = 0
-    y = 0 
-    while set(nodes_without_obst) - set(path): # untill not all nodes were visited 
+    def _go_left(self):
+        indx = self.y * self.x_shape + (self.x - 1)
+        self.x -= 1
+        self.path.append(indx)
 
-        if can_i_go_up(x, y, x_shape, grid, path):
-            x, y, path = going_up(x, y, grid, x_shape, y_shape, path)
+    def _find_the_closest_one(self) -> Tuple[int, int]:
+        indx = self.y * self.x_shape + self.x
+        filtered_connected_nodes_with_weights = [
+            (neighbor, self.complete_graph[indx][neighbor].get("weight", 1))
+            for neighbor in self.complete_graph.neighbors(indx)
+            if neighbor not in self.path
+        ]
+        next_indx, _ = min(filtered_connected_nodes_with_weights, key=lambda x: x[1])
 
-        elif y % 2 == 0:# right 
-            if x+1 < x_shape and grid[y, x+1] == 0 and y*x_shape + (x+1) not in path: #no obstyckle
-                indx = y*x_shape + (x+1)
-                path.append(indx)
-                x += 1
-            else: # obstyckle
-                # can_i_go_left():
-                # go_left()
-                if x > 0 and grid[y, x-1] == 0 and y*x_shape + (x-1) not in path: #no obstyckle
-                    indx = y*x_shape + (x-1)
-                    x -= 1
-                    path.append(indx)
+        self.y = next_indx // self.x_shape
+        self.x = next_indx % self.x_shape
 
-                elif can_i_go_down(x, y, y_shape, x_shape, grid, path):
-                    x, y, path = going_down(x, y, grid, x_shape, y_shape, path)
-                else: # there is obstyckle down or we aredy have been there 
-                    # go to the closest one 
-                    x, y, next_indx, indx = find_the_closest_one(x, y, x_shape, complete_graph, path)
-                    
-                    if 'path' not in complete_graph[indx][next_indx]:
-                        part_path = [next_indx]
-                    else:
-                        part_path = complete_graph[indx][next_indx]['path'][::-1][1:]
-                    path.extend(part_path)                  
+        return next_indx, indx
 
-        else:# left
-            if x > 0 and grid[y, x-1] == 0 and y*x_shape + (x-1) not in path: #no obstyckle
-                indx = y*x_shape + (x-1)
-                x -= 1
-                path.append(indx)
+    def _are_not_all_nodes_visited(self):
+        return self.nodes_without_obst - set(self.path)
 
-            else: # obstacle or end of board or we alredy have been there 
-                if x+1 < x_shape and grid[y, x+1] == 0 and y*x_shape + (x+1) not in path:# can i go right
-                    #go right
-                    indx = y*x_shape + (x+1)
-                    path.append(indx)
-                    x += 1
-                if can_i_go_down(x, y, y_shape, x_shape, grid, path):
-                    x, y, path = going_down(x, y, grid, x_shape, y_shape, path)
-                else: # there is obstyckle down or we aredy have been there 
-                # go to the closest one
-                    x, y, next_indx, indx = find_the_closest_one(x, y, x_shape, complete_graph, path)
-                    
-                    if 'path' not in complete_graph[indx][next_indx]:
-                        part_path = [next_indx]
-                    else:
-                        part_path = complete_graph[indx][next_indx]['path'][::-1][1:]
-                    path.extend(part_path)
-    
-    return path
+    def _zig_zag_path(self) -> List[int]:
+
+        while self._are_not_all_nodes_visited():
+
+            if self._can_i_go_up():
+                self._going_up()
+
+            elif self.y % 2 == 0:  # right
+
+                if self._can_i__go_right():
+                    self._go_right()
+
+                else:
+                    if self._can_i__go_left():
+                        self._go_left()
+
+                    elif self._can_i_go_down():
+                        self._going_down()
+
+                    else:  # there is obstyckle down or we aredy have been there, go to the closest one
+                        next_indx, indx = self._find_the_closest_one()
+
+                        if "path" not in self.complete_graph[indx][next_indx]:
+                            part_path = [next_indx]
+                        else:
+                            part_path = self.complete_graph[indx][next_indx]["path"][
+                                ::-1
+                            ][1:]
+
+                        self.path.extend(part_path)
+
+            else:  # left
+                if self._can_i__go_left():
+                    self._go_left()
+
+                else:
+                    if self._can_i__go_right():
+                        self._go_right
+
+                    if self._can_i_go_down():
+                        self._going_down()
+
+                    else:  # there is obstyckle down or we aredy have been there, go to the closest one
+
+                        next_indx, indx = self._find_the_closest_one()
+
+                        if "path" not in self.complete_graph[indx][next_indx]:
+                            part_path = [next_indx]
+                        else:
+                            part_path = self.complete_graph[indx][next_indx]["path"][
+                                ::-1
+                            ][1:]
+
+                        self.path.extend(part_path)
+
+        return self.path
+
+    def _initialize(
+        self, grid: np.ndarray, complete_graph: nx.Graph, nodes_without_obst: List[int]
+    ):
+        self.grid = grid
+        self.complete_graph = complete_graph
+        self.nodes_without_obst = set(nodes_without_obst)
+        self.x_shape, self.y_shape = grid.shape
+
+        self.path = [0]  # Start from the initial node (0,0)
+        self.x, self.y = 0, 0  # Starting coordinates
+
+    def __call__(
+        self, grid: np.ndarray, complete_graph: nx.Graph, nodes_without_obst: List[int]
+    ) -> List[int]:
+
+        self._initialize(grid, complete_graph, nodes_without_obst)
+        path = self._zig_zag_path()
+        return path
